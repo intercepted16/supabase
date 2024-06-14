19642d96422e7d9c321cc659f8ae9ddfcbb692cd
Fabrizio <fabri.feno@gmail.com>
2024-06-14 07:55:00 +0200

    Update apps/www/pages/storage/Storage.tsx
    Co-authored-by: Inian <inian1234@gmail.com>

--- apps/www/pages/storage/Storage.tsx
+++ apps/www/pages/storage/Storage.tsx
@@ -56,7 +56,10 @@ function StoragePage() {
             </span>,
           ]}
           subheader={[
-            <>An open source <b>S3 Compatible</b> Object Store, with unlimited scalability, for any file type.</>,
+            <>
+              An open source <b>S3 Compatible</b> Object Store, with unlimited scalability, for any
+              file type.
+            </>,
             'With custom policies and permissions that are familiar and easy to implement.',
           ]}
           image={[
@@ -114,7 +117,8 @@ function StoragePage() {
               <h4 className="h4">Dependable</h4>
 
               <p className="p">
-                Multiple Protocol Support: <br/> S3, TUS (Resumable Uploads), REST<br/>
+                Multiple Protocol Support: <br /> S3, Resumable Uploads and Standard Uploads
+                <br />
                 Enterprise-level scalability.
               </p>
             </div>
